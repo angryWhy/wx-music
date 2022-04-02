3f5c9b18abcda5dbcb442083e65e31a56d9e969d
angryWhy <875978822@qq.com>
2022-04-02 09:26:12 +0800

    item-component

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -13,7 +13,6 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log(111);
         wzxRequest.get("/top/mv",{offset:0,limit:10}).then(
             res=>{
                 console.log(res);
