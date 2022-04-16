ff879c4d972db131f5e2102ad0584aa3cb99269d
angryWhy <875978822@qq.com>
2022-04-16 21:09:54 +0800

    player

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -1,4 +1,4 @@
-import { getSearchHot } from "../../services/api-search"
+import { getSearchHot,getSearchResult,getSearchSuggest } from "../../services/api-search"
 
 // pages/detail-search/index.js
 Page({
@@ -7,7 +7,13 @@ Page({
      * 页面的初始数据
      */  
     data: {
-        hotKeywords:[]
+        hotKeywords:[],
+        suggestSongs:[],
+        searchValue:"",
+        suggestNodeValue:[],
+        resultSongs:[],
+        keyword:""
+
     },
 
     /**
@@ -19,8 +25,76 @@ Page({
     getPageData(){
         getSearchHot().then(res=>{
             this.setData({hotKeywords:res.data.result.hots})
-            console.log(res.data.result.hots);
         })
+    },
+    handleSearchAction(){
+        const searchValue = this.data.searchValue
+        getSearchResult(searchValue).then(res=>{
+            console.log(res.data.result.songs);
+            this.setData({resultSongs:res.data.result.songs})
+        })
+    },
+    handleSearchChange(e){
+        const resultValue = e.detail
+        this.setData({searchValue:resultValue})
+        if(!resultValue.length) {
+            this.setData({suggestSongs:[]})
+            this.setData({resultSongs:[]})
+            return
+        }
+        getSearchSuggest(resultValue).then(res=>{
+            const suggestSongs = res.data.result.allMatch
+             this.setData({suggestSongs})
+            const suggestKeywords = suggestSongs.map(item=>{
+                return item.keyword
+            })
+            const suggestNodeValue = []
+            for( let key of suggestKeywords){
+                const nodes = []
+                if(key.startsWith(resultValue)){
+                    const key1 = key.slice(0,resultValue.length)
+                    const node1 = {
+                        name:"span",
+                        attrs:{
+                            style:"color: red;"
+                        },
+                        children:[{type:"text",text:key1}]
+                    }
+                    nodes.push(node1)
+                    const key2 = key.slice(resultValue.length)
+                    const node2 = {
+                        name:"span",
+                        attrs:{
+                            style:"color:#000000;"
+                        },
+                        children:[{type:"text",text:key2}]
+                    }
+                    nodes.push(node2)
+                }else{
+                    const node = {
+                        name:"span",
+                        attrs:{style:"color:#000000;"},
+                        children:[{type:"text",text:key}]
+                }
+                nodes.push(node)
+            }
+            suggestNodeValue.push(nodes)
+        }
+            
+            this.setData({suggestNodeValue:suggestNodeValue})
+        })
+    },
+    handleSuggestItemClick(event){
+        const index = event.currentTarget.dataset.index
+        const keyword = this.data.suggestSongs[index].keyword
+        console.log( this.data.suggestSongs[index]);
+        this.setData({searchValue:keyword})
+
+
+    },
+    handleTagItemClick(event){
+        const keyword = event.currentTarget.dataset.item
+        this.setData({searchValue:keyword})
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
--- services/api-search.js
+++ services/api-search.js
@@ -3,6 +3,13 @@ import wzxRequest from "./index";
 export function getSearchHot(params) {
     return  wzxRequest.get("/search/hot")
 }
-export function getSearchSuggest(){
-    
+export function getSearchSuggest(keywords){
+    return wzxRequest.get("/search/suggest",{
+        keywords,
+        type:"mobile"
+
+    })
+}
+export function getSearchResult (keywords) {
+    return wzxRequest.get("/search",{keywords})
 }
